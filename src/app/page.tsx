dfae8493a1b5b785bdb2fcb92aef62373085b91e
Bryan Yeo <bryaniyeo3125@gmail.com>
2026-01-27 18:14:23 +0800

    Homepage added

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <main style={{ padding: 24 }}>
+            <h1>Food Tracker</h1>
+            <p>Deployed successfully! </p>
+        </main>
+    );
+}
